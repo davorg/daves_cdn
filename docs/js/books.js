2b3304714dd52818f9fea21c710990cbd0f06ee4
Dave Cross <dave@davecross.co.uk>
2025-04-24 10:17:37 +0100

    Add some more debugging

--- docs/js/books.js
+++ docs/js/books.js
@@ -36,6 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const scriptTag = document.currentScript;
   const url = scriptTag?.getAttribute("data-url") || "https://cdn.davecross.co.uk/data/books.json";
 
+  console.log(scriptTag);
+  console.log(scriptTag.getAttribute("data-tags-include"));
+  console.log(scriptTag.getAttribute("data-tags-exclude"));
+
   // Get include and exclude tags from the attributes, or default to no filtering
   const includeTags = scriptTag?.getAttribute("data-tags-include")?.split(",").map(tag => tag.trim()) || [];
   const excludeTags = scriptTag?.getAttribute("data-tags-exclude")?.split(",").map(tag => tag.trim()) || [];
