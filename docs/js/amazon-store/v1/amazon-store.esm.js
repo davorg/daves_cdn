5cecca5945bd64af5ef0bb449ac1621106dd82e9
davorg <24642+davorg@users.noreply.github.com>
2026-04-25 11:18:33 +0000

    amazon_store: publish v1.2.2 (update v1 alias: true)

--- docs/js/amazon-store/v1/amazon-store.esm.js
+++ docs/js/amazon-store/v1/amazon-store.esm.js
@@ -1,4 +1,4 @@
-/*! Amazon Store Router v1.2.1 — MIT */
+/*! Amazon Store Router v1.2.2 — MIT */
 const STORES = {
   AU:{name:"Australia",domain:"amazon.com.au"},
   BR:{name:"Brazil",domain:"amazon.com.br"},
